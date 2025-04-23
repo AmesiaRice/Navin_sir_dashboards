164e805b04300c9f630962db9da4286e18060a14
Rahul <developer@amasiarice.com>
2025-04-23 14:53:50 +0530

    filter run

--- src/app/lib/sheetData.js
+++ src/app/lib/sheetData.js
@@ -5,7 +5,7 @@ let cachedData = null;
 export async function getSheetData() {
   if (cachedData) return cachedData;
 
-  const res = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLicrgKmf11VlByw6QB3Wqm_yIyt7lJ9vzgT3AJbXswRy5xN0eC1raMEVDwCKqXJumzIARfMYvvgDn5gawd4LQtdpSL1lpz9UKbPQxpQi6dhxYcrE30Wji7LH2_9nsusqjICmjOJJwq293fX3P8ERHGiiPgyjPCH_PgiGwvYUPq83H7V2fZ_eTiUiG5VGP7tYeMFsg644gRgiMocye6RqkjNGq3juWPFQvLZJWIBrVPfsJuaxv9gKvN3Z1sdk_JHxLUWlGyfSJCH6y4UIu-LjCMK8_T_AA&lib=M0gYourf78sSuShXwOdZSKekTyRyPCM_0');
+  const res = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLhVB7kA85T0KgGyriDCyNWZ2nmxiiGoPmBurnUb6-tvdXiF0w17R1-LieE5rb2C12mO0H2amFi8CgKk7KjKNKhV7ei6wYrXEO22VEjWzCh8k9RYP1qsxAMxwTk-Ap5kHRq78XtUKrl5UsJbK6UrngrVtJH5DHP2BBQ2Q4yLheSaPxjFid6ji4A-dgpBd0c_LSllDmyde6axW5oDei5In3Nt1iWPtS5vLNpo1EdhdVhU_EgVlmdrwg7zeCxb4c1pW9FlSIS2r-I9W0U2Qn_ie50GxMc-8A&lib=M0gYourf78sSuShXwOdZSKekTyRyPCM_0');
 
   if (!res.ok) {
     throw new Error("Failed to fetch Google Sheet data");
